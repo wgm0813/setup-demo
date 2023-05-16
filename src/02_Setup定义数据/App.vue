aa5e47f8b758b1684f40cd2352fc96b88ac9615e
王高敏 <wanggaomin@greatmap.com.cn>
2023-05-16 22:24:31 +0800

    setup定义普通数据变量

--- src/02_Setup定义数据/App.vue
+++ src/02_Setup定义数据/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    <h2>message: {{ message }}</h2>
+    <button @click="changeBtn">修改</button>
+  </div>
 </template>
 
 <script>
 export default {
+  setup() {
+    // 定义普通的数据
+    // 缺点：数据不是响应式的
+    let message = "hello world"
 
+    const changeBtn = () => {
+      message = '王高敏'
+      console.log(message)
+    }
+    return  {message,changeBtn}
+  }
 }
 </script>
 
