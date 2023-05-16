ea601c32d149263824e41efe7321cecc757dcac8
王高敏 <wanggaomin@greatmap.com.cn>
2023-05-16 21:51:25 +0800

    setup举例

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <h2>当前计数: {{counter }}</h2>
+    <button @click="increment"> +1 </button>
+    <button @click="decrement"> -1 </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { ref } from 'vue'
+import useCounter from './hooks/useCounter';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    // 默认定义的数据都不是响应式数据
+    // let counter = ref(100);
+    // const increment = () => {
+    //   console.log('增加了');
+    //   counter.value++;
+    // }
+    // const decrement = () => {
+    //   console.log('减少了');
+    //   counter.value--;
+
+    const {counter,increment,decrement} = useCounter()
+    return {
+      counter,
+      increment,
+      decrement
+    }
+    // return {
+    //   ...useCounter()
+    // }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/hooks/useCounter.js
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+
+export default function useCounter() {
+  let counter = ref(100)
+  const increment = () => {
+    counter.value++
+    console.log(counter.value)
+  };
+  const decrement = () => {
+    counter.value--
+    console.log(counter.value)
+  }
+
+  return {
+    counter,
+    increment,
+    decrement
+  }
+}
