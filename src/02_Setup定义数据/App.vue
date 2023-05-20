88f110985478f4ec242a1133adae483c8ec126ee
王高敏 <wanggaomin@greatmap.com.cn>
2023-05-20 13:10:00 +0800

    定义响应式数据-reactive:复杂类型的数据，只能是复杂类型的数据

--- src/02_Setup定义数据/App.vue
+++ src/02_Setup定义数据/App.vue
@@ -2,21 +2,37 @@
   <div>
     <h2>message: {{ message }}</h2>
     <button @click="changeBtn">修改</button>
+    <hr>
+    
+    <h2>账号: {{ account.username }}</h2>
+    <h2>密码: {{ account.password }}</h2>
+    <button @click="changeName">修改账户</button>
   </div>
 </template>
 
 <script>
+import {reactive} from 'vue'
 export default {
   setup() {
     // 定义普通的数据
-    // 缺点：数据不是响应式的
+    // 缺点：数据不是响应式的,点击修改页面没有变化
     let message = "hello world"
-
     const changeBtn = () => {
       message = '王高敏'
       console.log(message)
     }
-    return  {message,changeBtn}
+    
+    //定义响应式数据
+    // reactive函数:定义复杂类型的数据
+    const account = reactive({
+      username: 'wanggaomin',
+      password: '123456'
+    });
+    const changeName = () => {
+      account.username = '王高敏'
+      console.log(account)
+    }
+    return  {message,changeBtn,account,changeName}
   }
 }
 </script>
