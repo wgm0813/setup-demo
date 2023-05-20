4fe1c4b0e307382dd126397cd722f7bf138d3ddb
王高敏 <wanggaomin@greatmap.com.cn>
2023-05-20 18:58:44 +0800

    ref响应式数据-浅层解包，使用时不需要.value,操作时要.value

--- src/02_Setup定义数据/App.vue
+++ src/02_Setup定义数据/App.vue
@@ -7,11 +7,25 @@
     <h2>账号: {{ account.username }}</h2>
     <h2>密码: {{ account.password }}</h2>
     <button @click="changeName">修改账户</button>
+    <hr>
+    
+    <!-- 默认情况下在tempalte中使用ref时，vue会自动对其进行解包（取出其中value，ref使用时不需要.value，在Setup操作时需要.value -->
+    <h2>当前技术：{{ counter }}</h2>
+    <button @click="add">+1</button>
+    <button @click="deAdd">-1</button>
+
+
+    <hr>
+    <!-- 使用的时候不需要写.value -->
+    <h2>当前计数： {{ info.counter }}</h2>
+    <!-- 修改的时候需要些.value -->
+    <button @click="info.counter.value++">+1</button>
+    <button @click="info.counter.value--">+1</button>
   </div>
 </template>
 
 <script>
-import {reactive} from 'vue'
+import {reactive, ref} from 'vue'
 export default {
   setup() {
     // 定义普通的数据
@@ -32,7 +46,22 @@ export default {
       account.username = '王高敏'
       console.log(account)
     }
-    return  {message,changeBtn,account,changeName}
+    
+    // 响应式数据
+    // ref函数：定义简单类型的数据（也可以定义复杂类型的数据）
+    const counter = ref(0);
+    const add = () => {
+      counter.value += 1
+    }
+    const deAdd = () => {
+      counter.value -= 1;
+    }
+
+    // ref时浅层解包
+    const info = {
+      counter
+    }
+    return  {message,changeBtn,account,changeName, counter, add, deAdd, info}
   }
 }
 </script>
