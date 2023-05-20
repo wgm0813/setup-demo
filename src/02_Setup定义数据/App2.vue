504c591398c7a0f2e444b2a320f1a6e246ec875b
王高敏 <wanggaomin@greatmap.com.cn>
2023-05-20 19:43:30 +0800

    ref和reactive的应用场景

---
+++ src/02_Setup定义数据/App2.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form>
+      账号：<input type="text" v-model="account.username">
+      密码：<input type="password" v-model="account.password">
+    </form>  
+  </div>
+</template>
+
+<script>
+import {reactive, ref,onMounted} from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    // 定义响应式数据：reactive/ref
+    // 强调：ref也可以定义复杂的数据
+    const info =ref({})
+
+    // reactive的应用场景
+    // reactive应用于本地的数据
+    // 多个数据之间有联系/关系（聚合的数据，组织在一起会有特定的作用）
+    const account = reactive({
+      username: 'Wanggaomin',
+      password: '123456'
+    })
+
+    // ref的应用场景：其他场景基本用ref(computed)
+    // 定义本地的一些简单数据
+    const message = ref('hello world')
+    const counter = ref(0)
+
+    // 定义从网络中获取的数据也是使用ref
+    // const musics = reactive([])
+    const musics = ref([])
+    onMounted(() => {
+      const serverMusics = ['1','2','3']
+      musics.value= serverMusics
+    })
+    return {info,account,message,counter,musics}
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 // import App from '../01_Setup函数的体验/App'
-import App from './02_Setup定义数据/App'
+// import App from './02_Setup定义数据/App'
+import App2 from './02_Setup定义数据/App2'
 
-createApp(App).mount('#app')
+createApp(App2).mount('#app')
