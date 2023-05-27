3ce1932d29d2f7daa421bd11681996f9efa7272d
王高敏 <wanggaomin@greatmap.com.cn>
2023-05-27 16:06:28 +0800

    setup中使用生命周期

---
+++ src/07_Setup生命周期函数/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>setup生命周期函数</div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  // vue2常见的生命周期选项
+  // created(){},
+  // beforeMount(){},
+  // mounted(){},
+  // beforeUpdate(){},
+
+  // vue3在setup里面使用生命周期
+  setup(){
+    // 需要注册别的生命周期
+    onMounted(() => {
+      console.log('onMounted')
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createApp } from 'vue'
 // import App from './03_Setup其他函数/App'
 // import App from './04_Setup-toRefs/App'
 // import App from './05_Setup中computed/App'
-import App from './06_Setup中ref引入元素/App'
+// import App from './06_Setup中ref引入元素/App'
+import App from './07_Setup生命周期函数/App'
 
 createApp(App).mount('#app')
